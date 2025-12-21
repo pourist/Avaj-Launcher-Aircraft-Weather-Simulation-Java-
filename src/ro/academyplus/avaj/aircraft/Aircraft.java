0d73c1bdecb3333d8c5e1d84bd7d81a42e573469
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-12-21 20:14:58 +0100

    feat(aircraft): implement tower registration in Aircraft

--- src/ro/academyplus/avaj/aircraft/Aircraft.java
+++ src/ro/academyplus/avaj/aircraft/Aircraft.java
@@ -1,16 +1,25 @@
 package ro.academyplus.avaj.aircraft;
 
 import ro.academyplus.avaj.weather.Coordinates;
+import ro.academyplus.avaj.tower.WeatherTower;
+
 
 public abstract class Aircraft implements Flyable {
 
 	protected long	 	id;
 	protected String	name;
 	protected Coordinates	coordinates;
+	protected WeatherTower	weatherTower;
 
 	protected	Aircraft(long p_id, String p_name, Coordinates p_coordinates) {
 		id = p_id;
 		name = p_name;
 		coordinates = p_coordinates;
+	}
+
+	@Override
+	public void	registerTower(final WeatherTower p_tower){
+		weatherTower = p_tower;
+		weatherTower.register(this);
 	}
 }
