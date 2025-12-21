7ae14bc525711de82c6c2a968d3fcd3476c6e7fc
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-12-21 20:24:03 +0100

    feat(aircraft): add concrete aircraft classes

---
+++ src/ro/academyplus/avaj/aircraft/Helicopter.java
@@ -0,0 +1,14 @@
+package	ro.academyplus.avaj.aircraft;
+
+import ro.academyplus.avaj.weather.Coordinates;
+
+public class Helicopter extends Aircraft {
+
+	public	Helicopter(long p_id, String p_name, Coordinates p_coordinate) {
+		super(p_id, p_name, p_coordinate);
+	}
+
+	@Override
+	public void	updateConditions() {
+	}
+}
