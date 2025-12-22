358a97b31ccd6e1d8b5a8d0707eccab4fc364815
ppour-ba <pouriya.pourbahrami@gmail.com>
2025-12-22 13:35:41 +0100

    feat(tower): implement WeatherTower concrete class

--- src/ro/academyplus/avaj/tower/Tower.java
+++ src/ro/academyplus/avaj/tower/Tower.java
@@ -1,11 +1,16 @@
 package ro.academyplus.avaj.tower;
 
+import java.util.ArrayList;
 import java.util.List;
 import ro.academyplus.avaj.aircraft.Flyable;
 
 public abstract class Tower {
     
     private List<Flyable>   observers;
+
+    protected Tower() {
+        observers = new ArrayList<>();
+    }
 
     public abstract void register(Flyable p_flyable);
 
--- src/ro/academyplus/avaj/tower/WeatherTower.java
+++ src/ro/academyplus/avaj/tower/WeatherTower.java
@@ -1,4 +1,28 @@
-    public void register(Flyable p_flyable) {
-        observers.add(p_flyable);
-        Syste
+package ro.academyplus.avaj.tower;
+
+import ro.academyplus.avaj.aircraft.Flyable;
+
+
+public class WeatherTower extends Tower {
+
+    public WeatherTower() {
+        super();
     }
+
+    @Override
+    public void register(Flyable p_flyable) {
+        super.addObserver(p_flyable);
+        //print to file
+    }
+
+    @Override
+    public void unregister(Flyable p_flyable) {
+        super.removeObserver(p_flyable);
+        //print to file
+    }
+
+    @Override
+    protected void conditionChanged() {
+
+    }
+}
